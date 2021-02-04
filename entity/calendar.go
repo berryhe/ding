cba522016f9112b331f7da8186226ec6e08e7b5b
Berry.he <berry.he@trustasia>
2021-02-04 17:25:16 +0800

    chore: 钉日程接口更新

--- entity/calendar.go
+++ entity/calendar.go
@@ -41,17 +41,17 @@ type (
 	}
 	// CalendarCreateResult 创建日程的消息主体
 	CalendarCreateResult struct {
-		Attendees        Attendees `json:"attendees"`
-		CalendarID       string    `json:"calendar_id"`
-		Description      string    `json:"description"`
-		End              Time      `json:"end"`
-		EventID          string    `json:"event_id"`
-		Location         Location  `json:"location"`
-		NotificationType string    `json:"notification_type"`
-		Organizer        Organizer `json:"organizer"`
-		Reminder         Reminder  `json:"reminder"`
-		Start            Time      `json:"start"`
-		Summary          string    `json:"summary"`
+		Attendees        []AttendeesType `json:"attendees"`
+		CalendarID       string          `json:"calendar_id"`
+		Description      string          `json:"description"`
+		End              Time            `json:"end"`
+		EventID          string          `json:"event_id"`
+		Location         Location        `json:"location"`
+		NotificationType string          `json:"notification_type"`
+		Organizer        Organizer       `json:"organizer"`
+		Reminder         Reminder        `json:"reminder"`
+		Start            Time            `json:"start"`
+		Summary          string          `json:"summary"`
 	}
 )
 
@@ -64,26 +64,26 @@ type (
 	}
 	// Event 消息主体
 	Event struct {
-		EventID     string      `json:"event_id"`
-		Summary     string      `json:"summary"`
-		CalendarID  string      `json:"calendar_id"`
-		Description string      `json:"description"`
-		Attendees   []Attendees `json:"attendees"`
-		Reminder    Reminder    `json:"reminder"`
-		Organizer   Organizer   `json:"organizer"`
-		Start       Time        `json:"start"`
-		End         Time        `json:"end"`
-		Location    Location    `json:"location"`
+		EventID     string          `json:"event_id"`
+		Summary     string          `json:"summary"`
+		CalendarID  string          `json:"calendar_id"`
+		Description string          `json:"description"`
+		Attendees   []AttendeesType `json:"attendees"`
+		Reminder    Reminder        `json:"reminder"`
+		Organizer   Organizer       `json:"organizer"`
+		Start       Time            `json:"start"`
+		End         Time            `json:"end"`
+		Location    Location        `json:"location"`
 	}
 	// Reminder 会议开始前提醒
 	Reminder struct {
 		Method  string `json:"method"`
-		Minutes string `json:"minutes"`
+		Minutes int    `json:"minutes"`
 	}
 	// Time 开始与结束时间
 	Time struct {
 		Timezone  string `json:"timezone"`
-		Timestamp int    `json:"timestamp"`
+		Timestamp int64  `json:"timestamp"`
 	}
 	// Organizer 日程组织者信息。
 	Organizer struct {
@@ -96,9 +96,8 @@ type (
 		Longitude string `json:"longitude"`
 	}
 	// Attendees 日程参与者，参与者最大人数为100，包括组织者
-	Attendees struct {
-		UserID         string `json:"userid"`
-		AttendeeStatus string `json:"attendee_status"`
+	AttendeesType struct {
+		UserID string `json:"userid"`
 	}
 	// CalendarUpdateResp 日程更新返回
 	CalendarUpdateResp struct {
@@ -112,10 +111,10 @@ type (
 type (
 	// CalendarAttendeeUpdateRequest 更新日程参与者请求
 	CalendarAttendeeUpdateRequest struct {
-		AgentID    int         `json:"agentid"`
-		EventID    string      `json:"event_id"`
-		CalendarID string      `json:"calendar_id"`
-		Attendees  []Attendees `json:"attendees"`
+		AgentID    int             `json:"agentid"`
+		EventID    string          `json:"event_id"`
+		CalendarID string          `json:"calendar_id"`
+		Attendees  []AttendeesType `json:"attendees"`
 	}
 	// CalendarAttendeeUpdateResp 更新日程参与者返回
 	CalendarAttendeeUpdateResp struct {
